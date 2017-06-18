c43f02b42b0da8c2e1e2ecb314714953480622b9
Ingu Kang <igxactly@gmail.com>
2017-06-18 20:15:36 +0900

    Add more example code lines

--- example.sh
+++ example.sh
@@ -3,9 +3,11 @@
 source $(dirname $0)/argparse.bash || exit 1
 argparse "$@" <<EOF || exit 1
 parser.add_argument('infile')
-parser.add_argument('outfile')
+parser.add_argument('outfile', nargs='?', default='out.txt', help='output file')
 parser.add_argument('-a', '--the-answer', default=42, type=int,
                     help='Pick a number [default %(default)s]')
+parser.add_argument('-q', '--the-question', default=24, type=int,
+                    help='Pick a number')
 parser.add_argument('-m', '--multiple', nargs='+',
                     help='multiple values allowed')
 EOF
